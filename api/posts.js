c6525a570c77dfcf9b0744db7a3b28d626b4e8db
da-broussard <dustin.a.broussard@gmail.com>
2022-07-31 18:17:53 -0500

    day 3 finished

--- api/posts.js
+++ api/posts.js
@@ -66,13 +66,10 @@ postsRouter.patch('/:postId', requireUser, async (req, res, next) => {
       const updatedPost = await updatePost(postId, updateFields);
       res.send({ post: updatedPost })
     } else {
-      next({
-        name: 'UnauthorizedUserError',
-        message: 'You cannot update a post that is not yours'
-      })
+      
     }
-  } catch ({ name, message }) {
-    // next({ name, message });
+  } catch (error) {
+   console.error(error)
   }
 });
 
